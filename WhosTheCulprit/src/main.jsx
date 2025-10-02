868e93a5f764cd59d5d2e151d85c5e65ec009ea8
Yyrion <ewrondin@gmail.com>
2025-10-02 16:20:15 +0200

    Added Sentry

--- WhosTheCulprit/src/main.jsx
+++ WhosTheCulprit/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://679b5f930431173fe13446db94a755c3@o4510120291926016.ingest.de.sentry.io/4510120293302352",
+  sendDefaultPii: false
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
