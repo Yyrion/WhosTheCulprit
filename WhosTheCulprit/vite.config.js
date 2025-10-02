99101962f555c0949bdf4b67059960124dc502e5
Yyrion <ewrondin@gmail.com>
2025-10-02 16:46:55 +0200

    push

--- WhosTheCulprit/vite.config.js
+++ WhosTheCulprit/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    sourcemap: true, // Source map generation must be turned on
+  },
+  plugins: [react(),
+    sentryVitePlugin({
+      org: "yyrion",
+      project: "javascript-react",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    }),
+  ],
 })
